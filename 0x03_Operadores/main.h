e1dc8feee4f6c301325a86c0a70609ed227dda59
RodriGomez53 <112216718+RodriGomez53@users.noreply.github.com>
2022-09-27 13:51:44 -0500

    Add files via upload

---
+++ 0x03_Operadores/main.h
@@ -0,0 +1,13 @@
+
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+float perimeter(float length, float width);
+float area(float length, float width);
+float areaC(float radius);
+float diameterC(float radius);
+float circumferenceC(float radius);
+float CMtoKM(float cm);
+float CMtoM(float cm);
+
+#endif
