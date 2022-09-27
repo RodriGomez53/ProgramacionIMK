f086a659283c41d80c590d1c12f63599c0dafd41
RodriGomez53 <112216718+RodriGomez53@users.noreply.github.com>
2022-09-27 07:17:19 -0500

    Add files via upload

---
+++ 0x01_Funciones/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * Calculadora
+ * Return: 0 if succeed
+ * 
+ */
+int main(void){
+      int suma = 0;
+    
+    suma = add(12,10);
+    printf("El valor de la suma es: %d\n",suma);
+    return 0;
+}
+
