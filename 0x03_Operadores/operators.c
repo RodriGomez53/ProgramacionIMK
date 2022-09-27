62f1d21d01964916a43ebe71490b12b54148943b
RodriGomez53 <112216718+RodriGomez53@users.noreply.github.com>
2022-09-27 09:36:24 -0500

    Add files via upload

---
+++ 0x03_Operadores/operators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+
+*/
+int main()
+{int num1, num2;
+int sum, sub, mult, mod;
+float div;
+
+printf("Enter any two number: ");
+scanf("%d %d",&num1,%num2);
+
+sum = num1 + num2;
+sub = num1 + num2;
+sum = num1 + num2;
+sum = num1 + num2;
+sum = num1 + num2;
+
+}
